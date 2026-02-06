fcce019d211bc747adfa00a9c59e8d9c7f616f3d
Dev Ashish <dshish719@gmail.com>
2026-02-06 14:41:50 +0530

    fixed

--- 11Backend/src/index.js
+++ 11Backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "leet-code-master-frontend.vercel.app",
+    origin: "https://leet-code-master-backend.vercel.app/",
     credentials: true,
   }),
 );
